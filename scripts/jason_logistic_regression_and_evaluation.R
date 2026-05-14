d8e516010fcd8676c0acd31668c8e9c51b7c9953
Jason Pham <Jason@MacBook-Air.local>
2026-05-13 23:50:50 -0700

    Update models with macroeconomic indicators

--- scripts/jason_logistic_regression_and_evaluation.R
+++ scripts/jason_logistic_regression_and_evaluation.R
@@ -1,8 +1,11 @@
 library(dplyr)
-library(janitor)
 library(ggplot2)
+library(gt)
 library(pROC)
 library(pscl)
+
+# Set working directory
+setwd("~/Desktop/MATH 167R/bank-marketing")
 
 # ------------------------- Load cleaned data set -------------------------
 source("scripts/01_data_cleaning_and_eda.R")
@@ -31,13 +34,14 @@ test_data <- data[-train_index, ]
 # ------------------------- Base logistic regression model -------------------------
 model_base <- glm(y_binary ~ age + job + marital + education + default + housing + loan + contact + 
                     month + day_of_week + 
-                    campaign + previous_contact + previous + poutcome,
+                    campaign + previous_contact + previous + poutcome + emp.var.rate +
+                    cons.price.idx + cons.conf.idx + euribor3m + nr.employed,
                     data = train_data,
                     family = "binomial")
 summary(model_base)
 base_percent_devReduction <- (model_base$null.deviance - model_base$deviance) / model_base$null.deviance
 base_percent_devReduction
-# [NOTE]: Base model with no interaction improves deviance by ~16.94%
+# [NOTE]: Base model with no interaction improves deviance by ~21.89%
 
 
 # ------------------------- Predict using base model for a comparison baseline ------------------------- 
@@ -67,8 +71,8 @@ base_sensitivity <- TP / (TP + FN)
 base_specificity <- TN / (TN + FP)
 base_sensitivity
 base_specificity
-# [NOTE]: Base model is decently accurate at predicting NO (specificity ~86.53%)
-# but worse at predicting YES (sensitivity ~53.35%)
+# [NOTE]: Base model is decently accurate at predicting NO (specificity ~88.03%)
+# but worse at predicting YES (sensitivity ~60.93%)
 
 
 # ------------------------- Base ROC curve -------------------------
@@ -82,9 +86,9 @@ base_auc <- auc(roc_base_obj)
 
 # Graph base ROC curve
 base_roc <- ggplot(roc_base_df, aes(x = fpr, y = tpr)) +
-                  geom_line(color = "pink", linewidth = 1) +
+                  geom_line(color = "lightblue", linewidth = 1) +
                   geom_abline(linetype = "dashed", color = "darkgrey") +
-                  labs(title = paste0("Base ROC Curve (AUC = ", round(base_auc, 3), ")"),
+                  labs(title = paste0("Base ROC Curve (AUC = ", round(base_auc, 5), ")"),
                        x = "False Positive Rate",
                        y = "True Positive Rate") +
                   theme_light()
@@ -94,8 +98,8 @@ base_roc
 ggsave(
   filename = "plots/BaseROC.png",
   plot = base_roc,
-  width = 6,
-  height = 5,
+  width = 10,
+  height = 9,
   dpi = 300
 )
 
@@ -109,21 +113,26 @@ model_interact <- glm(y_binary ~ age + job + marital + education + default + hou
                         education * job +
                         age * campaign +
                         default * loan +
-                        campaign * loan,
+                        campaign * loan +
+                        euribor3m * cons.conf.idx +
+                        euribor3m * cons.price.idx +
+                        emp.var.rate * cons.conf.idx +
+                        euribor3m * emp.var.rate,
                       data = train_data,
                       family = "binomial")
 
 # Selecting meaningful factors using backward selection
 model_selected <- step(model_interact, direction = "backward")
+formula(model_selected)
 
 # Comparing percentage of deviance reduction between based and selected interaction model
 summary(model_selected)
 selected_percent_devReduction <- (model_selected$null.deviance - model_selected$deviance) / model_selected$deviance
 selected_percent_devReduction
 AIC(model_base, model_selected)
-# [NOTE]: Interaction model improves deviance by ~20.27%, so about 3.33% improvement
-# Interaction model reduced AIC by 4.0 compared to base model
-# Also reduced df by 10 (47 -> 37), suggesting the selected interaction model is better fitting, less complex with fewer variables used
+# [NOTE]: Interaction model improves deviance by ~28.09%, so about 6.2% improvement
+# Interaction model has a lower AIC (~29.24 lower than  base model)
+# Also reduced df by 11 (52 -> 41), suggesting the selected interaction model is better fitting, yet less complex with fewer variables used
 
 
 # ------------------------- Predict using selected interaction model -------------------------
@@ -153,7 +162,7 @@ selected_sensitivity <- TP / (TP + FN)
 selected_specificity <- TN / (TN + FP)
 selected_sensitivity
 selected_specificity
-# [NOTE]: Interaction model improved sensitivity by ~1.02% but reduced specificity by ~1.24%
+# [NOTE]: Interaction model has specificity ~87.62 (~0.41 lower compared to base) and sensitivity ~61.36 (~0.43 higher than base)
 
 # Combining the two ROC data frames into one
 roc_selected_df <- data.frame(tpr = roc_selected_obj$sensitivities,
@@ -167,7 +176,7 @@ compare_roc <- ggplot(roc_combined, aes(x = fpr, y = tpr, color = model, linewid
                       geom_line() +
                       geom_abline(linetype = "dashed", color = "darkgrey") +
                       scale_color_manual(
-                        values = c("Base Model" = "pink", "Selected Interaction Model" = "darkblue")
+                        values = c("Base Model" = "lightblue", "Selected Interaction Model" = "black")
                       ) +
                       scale_linewidth_manual(
                         values = c("Base Model" = 2, "Selected Interaction Model" = 0.6) 
@@ -185,18 +194,30 @@ compare_roc
 ggsave(
   filename = "plots/compareROC.png",
   plot = compare_roc,
-  width = 6,
-  height = 5,
+  width = 10,
+  height = 9,
   dpi = 300
 )
 
-# Finding summary table
+# ------------------------- Model Performance Summary -------------------------
 final_model_comparison <- data.frame(Deviance_Reduction = c(base_percent_devReduction, selected_percent_devReduction), 
                                      Pseudo_R2 = c(round(pR2(model_base)["McFadden"], 5), round(pR2(model_selected)["McFadden"], 5)), Sensitivity = c(base_sensitivity,
                                      selected_sensitivity), Specificity = c(base_specificity, selected_specificity), AUC = c(base_auc, selected_auc))
 colnames(final_model_comparison) <- c("Deviance Reduction [%]", "Pseudo R^2", "Sensitivity", "Specificity", "AUC")
 rownames(final_model_comparison) <- c("Base Additive", "Selected Interaction")
 View(final_model_comparison)
+
+# ------------------------- Significant Predictors Summary -------------------------
+coef_table <- summary(model_selected)$coefficients
+coef_df <- as.data.frame(coef_table)
+coef_df$Significant_Variable <- rownames(coef_df)
+rownames(coef_df) <- NULL
+coef_df <- coef_df %>%
+  rename(`p_value < 0.05` = `Pr(>|z|)`) %>%
+  mutate(`p_value < 0.05` = round(`p_value < 0.05`, 6)) %>%
+  filter(`p_value < 0.05` < 0.05, Significant_Variable != "(Intercept)") %>%
+  select(Significant_Variable, everything()) 
+View(coef_df)
 
 
 
